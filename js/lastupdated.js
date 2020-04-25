28b99418fa4c86413e116de4ebe92fe13a03b14a
juarez18 <juarez18@byui.edu>
2020-04-25 16:12:59 -0600

    Update

--- js/lastupdated.js
+++ js/lastupdated.js
@@ -7,7 +7,7 @@ try{
         year: "numeric"
     };
     document.getElementById(
-        "updateddate"
+        "udate"
     ).textContent = new Date().toLocaleDateString("en-US", options);
 } catch(e){
     alert("Error with code or your browser does not support Locale");
