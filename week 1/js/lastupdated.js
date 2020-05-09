f4aaf5d87838687eb285030fad2ea893bcaae9b9
juarez18 <juarez18@byui.edu>
2020-05-09 00:24:28 -0600

    Update

--- week 1/js/lastupdated.js
+++ week 1/js/lastupdated.js
@@ -1,5 +1,33 @@
-let date = new Date(document.lastModified);
+let daynames=[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months= [
+    "January",
+    "Feb",
+    "Mar",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
 
-document.getElementById(udate).textContent = date;
+let d= new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + ', ' + monthName + ' ' + d.getDay() + ", " + year;
+
+document.getElementById(udate).textContent = fulldate;
 
 
