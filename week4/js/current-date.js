b44c4a375c55e47665d4f83b264fc23dc2fef278
juarez18 <juarez18@byui.edu>
2020-05-26 17:59:39 -0600

    update

--- week4/js/current-date.js
+++ week4/js/current-date.js
@@ -5,10 +5,9 @@ try{
         month: "long",
         year: "numeric"
     };
-
 document.getElementById(
-    "current-date"
-    ).innerHTML = new Date().toLocaleDateString(en-US, options);
+    "currentdate"
+    ).textContent = new Date().toLocaleDateString(en-US, options);
 
 } catch (e){
     alert("Error with code or your browser does not support Locale 1");
