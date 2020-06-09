e9da802966abab74574f26ee8786344e03593b6b
juarez18 <juarez18@byui.edu>
2020-06-08 23:44:14 -0600

    update

--- week5/js/alert.js
+++ week5/js/alert.js
@@ -1,1 +1,9 @@
-if 
+let today = new Date();
+    let day = today.getDay();
+    var daylist = ["Sunday" , "Monday" , "Tuesday" ,
+                    "Wednesday" , "Thursday" , "Friday" ,
+                    "Saturday"]
+
+    if(daylist[day] == "Friday"){
+        
+    }
