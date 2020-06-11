2a1cef13a47fc1597d971527351587fed244ed26
juarez18 <juarez18@byui.edu>
2020-06-10 23:59:52 -0600

    update

--- week6/js/windchill.js
+++ week6/js/windchill.js
@@ -4,9 +4,13 @@ var b = document.getElementById("windSpeed");
 var t = a.innerHTML.match(/\d+/g); 
 var s = b.innerHTML.match(/\d+/g); 
 
-
+if(t <= 50 && s >= 3){
 var f = 35.74 + .6215 * t - 35.65 * Math.pow(s, 0.15) + .4275 * t * Math.pow(s , .16);
+Math.round(f);
 
-Math.trunc(f);
+}
 
+else{
+    var f = "NA"
+}
 document.getElementById("windChill").innerHTML = f;
