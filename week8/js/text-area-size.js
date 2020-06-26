6c3e7a3f9a35a0fbaddffc970dc186c41c973ba0
juarez18 <juarez18@byui.edu>
2020-06-25 18:23:05 -0600

    update

--- week8/js/text-area-size.js
+++ week8/js/text-area-size.js
@@ -1,6 +1,6 @@
 w = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
 var targetMedWidth = 1150;
-var targgetSmallWidth = 750;
+var targetSmallWidth = 750;
 if ( w >= targetMedWidth){     
     document.getElementById("info-area").columns = 80;
 }
