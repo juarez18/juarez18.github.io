fbd4268713bff1fd005533f975833f1811812d1d
juarez18 <juarez18@byui.edu>
2020-07-06 21:53:57 -0600

    update

--- week6/js/windchill.js
+++ week6/js/windchill.js
@@ -7,7 +7,7 @@ var s = b.innerHTML.match(/\d+/g);
 if(t <= 50 && s >= 3){
 var x = 35.74 + (.6215 * t) - (35.65 * Math.pow(s, 0.15)) + (.4275 * t * Math.pow(s , .16));
 
-var f = Math.round(x);
+var f = Math.round(x) + "&#176;F";
 
 }
 
