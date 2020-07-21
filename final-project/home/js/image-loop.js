e5693ad96ee9e4e85503a68a8684fd5fcba40e55
juarez18 <juarez18@byui.edu>
2020-07-21 16:13:22 -0600

    update

---
+++ final-project/home/js/image-loop.js
@@ -0,0 +1,24 @@
+var i = 0;
+
+var images = [];
+var time = 5000;
+
+images[0] = "/final-project/home/images/banner-img-1.jpg"
+images[1] = "/final-project/home/images/banner-img-2.jpg"
+images[2] = "/final-project/home/images/banner-img-3.jpg"
+
+function changeImg(){
+    document.slide.src = images[i];
+
+    if(i < images.length - 1){
+        i++;
+    }
+    else{
+        i = 0;
+    }
+
+
+    setTimeout("changeImg()" , time);
+}
+
+window.onload = changeImg;
